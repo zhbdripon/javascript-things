bede546002e8fdac49911e10a6db776f0d62db84
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2025-02-01 23:37:27 +0600

    leetcode: 59. Spiral Matrix II

---
+++ problem-solving/dsa/spiralMatrix2.js
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/spiral-matrix-ii/
+
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+
+    let resArray = Array.from({ length: n}, () => Array(n).fill(0))
+
+    let dx = [1,0, -1, 0]
+    let dy = [0, 1, 0, -1]
+
+    let adx = [0, 1,0, -1]
+    let ady = [-1, 0, 1, 0]
+
+    let asend = false;
+    let cx = 0, cy = 0, nx, ny, curval = 1;
+
+    resArray[0][0] = curval++;
+
+    const nextMove = (x, y) => {
+        if (!asend) {
+            for (let i = 0; i < 4; i++) {
+                let xx = x + dx[i], yy = y + dy[i];
+
+                if (xx < n && xx >=0 && yy < n && yy >=0 && resArray[yy][xx] === 0) {
+                    nx = xx;
+                    ny = yy;
+
+                    if (i===3) asend = true;
+                    return true;
+                }
+            }
+        }else {
+            for (let i = 0; i < 4; i++) {
+                let xx = x + adx[i], yy = y + ady[i];
+
+                if (xx < n && xx >=0 && yy < n && yy >=0 && resArray[yy][xx] === 0) {
+                    nx = xx;
+                    ny = yy;
+
+                    if (i!==0) asend = false;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    while(nextMove(cx, cy)) {
+        resArray[ny][nx] = curval++;
+        cx = nx;
+        cy = ny;
+    }
+
+    return resArray;
+};
