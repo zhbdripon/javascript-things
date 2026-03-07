c1ad4d5cc9db865058089f4ed4bcf907e7d4bb71
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2026-03-08 02:07:21 +0600

    refactor(redisAdapter): standardize job handling and improve code clarity

--- jobQueue/src/redisAdapter.ts
+++ jobQueue/src/redisAdapter.ts
@@ -1,4 +1,4 @@
-import { Redis as IORedis} from "ioredis";
+import { Redis as IORedis } from "ioredis";
 import { Queue, type QueueOptions, type Job } from "./queue.js";
 
 export class RedisQueue<T> extends Queue<T> {
@@ -12,59 +12,60 @@ export class RedisQueue<T> extends Queue<T> {
     this.redisNonBlocking = new IORedis(redisUrl);
     this.redisSubscriber = new IORedis(redisUrl);
 
-    this.listenForExpiredJobs()
-    this.checkForExpiredJobs()
+    this.listenForExpiredJobs();
+    this.checkForExpiredJobs();
   }
 
   private async listenForExpiredJobs() {
-    await this.redisSubscriber.subscribe("__keyevent@0__:expired")
+    await this.redisSubscriber.subscribe("__keyevent@0__:expired");
 
     this.redisSubscriber.on("message", async (channel, message) => {
-      if (channel !== "__keyevent@0__:expired") return
-      if (!message.startsWith(this.expiringJobIdList(""))) return
+      if (channel !== "__keyevent@0__:expired") return;
+      if (!message.startsWith(this.expiringJobIdListQueue(""))) return;
 
-      const jobId = message.split(":").pop()
-      if (jobId == null) return
+      const jobId = message.split(":").pop();
+      if (jobId == null) return;
 
-      await this.moveJobToActive(jobId)
-    })
+      await this.moveJobToActive(jobId);
+    });
   }
 
   private async checkForExpiredJobs() {
     const expiredJobs = await this.redisNonBlocking.zrangebyscore(
-      this.delayedJobIdsQueue,
+      this.scheduledJobIdsQueue,
       0,
-      Date.now()
-    )
+      Date.now(),
+    );
 
     for (const jobId of expiredJobs) {
-      await this.moveJobToActive(jobId)
+      await this.moveJobToActive(jobId);
     }
   }
 
   private async moveJobToActive(jobId: string) {
-    const job = await this.redisNonBlocking.hget(this.delayedJobsList, jobId)
-    if (job == null) return
+    const job = await this.redisNonBlocking.hget(this.scheduledJobsList, jobId);
+    if (job == null) return;
 
-    await this.addActiveJob(JSON.parse(job))
+    await this.addActiveJob(JSON.parse(job));
     await this.redisNonBlocking
       .multi()
-      .zrem(this.delayedJobIdsQueue, jobId)
-      .hdel(this.delayedJobsList, jobId)
-      .exec()
+      .zrem(this.scheduledJobIdsQueue, jobId)
+      .hdel(this.scheduledJobsList, jobId)
+      .exec();
   }
 
   protected async push(job: Job<T>) {
+    const expiringJobIdListQueue = this.expiringJobIdListQueue(job.id);
     if (job.options.delay > 0) {
       return await this.redisNonBlocking
         .multi()
-        .set(this.expiringJobIdList(job.id), job.id)
-        .pexpire(this.expiringJobIdList(job.id), job.options.delay)
-        .zadd(this.delayedJobIdsQueue, Date.now() + job.options.delay, job.id)
-        .hset(this.delayedJobsList, job.id, JSON.stringify(job))
-        .exec()
+        .set(expiringJobIdListQueue, job.id)
+        .pexpire(expiringJobIdListQueue, job.options.delay)
+        .zadd(this.scheduledJobIdsQueue, Date.now() + job.options.delay, job.id)
+        .hset(this.scheduledJobsList, job.id, JSON.stringify(job))
+        .exec();
     } else {
-      return await this.addActiveJob(job)
+      return await this.addActiveJob(job);
     }
   }
 
@@ -72,32 +73,32 @@ export class RedisQueue<T> extends Queue<T> {
     return await this.redisNonBlocking.zadd(
       this.activeJobsQueue,
       (job.options.priority ?? 0) * -1,
-      JSON.stringify(job)
-    )
+      JSON.stringify(job),
+    );
   }
 
   async getOrWaitForJob() {
-    const data = await this.redisBlocking.bzpopmin(this.activeJobsQueue, 0)
-    return data?.[1] ? JSON.parse(data[1]) : null
+    const data = await this.redisBlocking.bzpopmin(this.activeJobsQueue, 0);
+    return data?.[1] ? JSON.parse(data[1]) : null;
   }
 
   get length() {
-    return this.redisNonBlocking.zcard(this.activeJobsQueue)
+    return this.redisNonBlocking.zcard(this.activeJobsQueue);
+  }
+
+  private expiringJobIdListQueue(jobId: string) {
+    return `${this.queueName}:expiringJobs:${jobId}`;
   }
 
   private get activeJobsQueue() {
-    return `${this.queueName}:active`
+    return `${this.queueName}:active`;
   }
 
-  private expiringJobIdList(jobId: string) {
-    return `${this.queueName}:expiringJobs:${jobId}`
+  private get scheduledJobIdsQueue() {
+    return `${this.queueName}:delayed`;
   }
 
-  private get delayedJobIdsQueue() {
-    return `${this.queueName}:delayed`
-  }
-
-  private get delayedJobsList() {
-    return `${this.queueName}:jobs`
+  private get scheduledJobsList() {
+    return `${this.queueName}:jobs`;
   }
 }
