6b938bee938556d9dcdfd9fe28a30cabde06b831
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2026-04-03 23:48:00 +0600

    feat: add reverse linked List solution

---
+++ problem-solving/dsa/linkedList/reverseLinkedList.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+
+  const newHead = reverseList(head.next);
+
+  head.next.next = head;
+  head.next = null;
+
+  return newHead;
+};
