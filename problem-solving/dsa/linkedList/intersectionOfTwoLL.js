b16b77fd818a2408379b3cc1e021315d9902f306
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2026-04-07 01:17:57 +0600

    feat: add solution of intersection of two list

---
+++ problem-solving/dsa/linkedList/intersectionOfTwoLL.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let curA = headA;
+  let curB = headB;
+
+  while (curA !== curB) {
+    curA = curA ? curA.next : headB;
+    curB = curB ? curB.next : headA;
+  }
+
+  return curA;
+};
+
+
+
