bffc6827227233ca7eb7c3bef30932bb77fda1dc
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2026-05-07 10:31:42 +0600

    feat: implement MyQueue class using two stacks for queue operations

---
+++ problem-solving/dsa/stackAndQueue/implementQueueUsingStacks.js
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/implement-queue-using-stacks/
+
+var MyQueue = function () {
+  this.input = [];
+  this.output = [];
+
+  this._move = () => {
+    if (this.output.length === 0) {
+      while (this.input.length > 0) {
+        this.output.push(this.input.pop());
+      }
+    }
+  };
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.input.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  this._move();
+  return this.output.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  this._move();
+  return this.output.at(-1);
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.input.length === 0 && this.output.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
