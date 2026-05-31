c34ec7a8c5a612e3a5635e0f8980fba0ee938d48
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2026-05-31 12:16:00 +0600

    refactor: simplify rob function logic for house robber II problem

--- problem-solving/dsa/DynamicProgramming/houseRover2.js
+++ problem-solving/dsa/DynamicProgramming/houseRover2.js
@@ -3,30 +3,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    if (nums.length < 4) {
-        return Math.max(...nums)
-    }
-    
-    const solve = (startInd, lastInd) => {
-        const startVal = nums[startInd];
-        const dp = Array(lastInd + 1).fill(0);
-    
-    for ( let ind = startInd; ind <= lastInd; ind++){
-        let cur = nums[ind];
-        if(ind === 0 || ind === 1){
-            dp[ind] = startVal;
-            continue;
-        }
-        
-        const maxWithCur = (ind - 2 === 0 && ind === nums.length - 1) ? 0 : cur + dp[ind - 2];
-        
-        dp[ind] = Math.max(dp[ind - 1], maxWithCur);
-    }
-    
-    return dp[lastInd]
+var rob = function (nums) {
+  const len = nums.length;
+
+  const solve = (startInd, lastInd) => {
+    const startVal = nums[startInd];
+    const dp = Array(lastInd + 1).fill(0);
+
+    for (let ind = startInd; ind <= lastInd; ind++) {
+      let cur = nums[ind];
+      if (ind === 0 || ind === 1) {
+        dp[ind] = startVal;
+        continue;
+      }
+
+      dp[ind] = Math.max(dp[ind - 1], cur + dp[ind - 2]);
     }
 
-    const len = nums.length;
-    return Math.max(solve(0, len - 2), solve(1, len - 1))
+    return dp[lastInd];
+  };
+
+  return len === 1 ? nums[0] : Math.max(solve(0, len - 2), solve(1, len - 1));
 };
